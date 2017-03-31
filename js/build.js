1d44d1840ea2d26d00beffe022b2e75dd2676223
Hugo Carneiro <hcarneiro@weboo.biz>
2017-03-31 12:43:06 +0100

    prepared gallery code for enhancement in PhotoSwipe

--- js/build.js
+++ js/build.js
@@ -26,38 +26,8 @@ Fliplet.Widget.instance('image-gallery', function (data) {
     if(!Fliplet.Env.get('interact')) {
       $(WALL_SELECTOR + ' .brick img').click(function () {
         var $clickedBrick = $(this)[0].parentElement;
-        var gallery = openPhotoSwipe($clickedBrick.index - 1);
+        Fliplet.Navigate.previewImages(data, {index: $clickedBrick.index - 1})
       });
-
-      function openPhotoSwipe(index) {
-        var items = $(WALL_SELECTOR).find('.brick img');
-
-        items = items.map(function(i, img) {
-          return {
-            src: img.src,
-            title: img.alt,
-            w: img.naturalWidth,
-            h: img.naturalHeight
-          };
-        });
-
-        var options = {
-          history: false,
-          focus: false,
-          closeOnScroll: false,
-          shareEl: false,
-          fullscreenEl: false,
-          zoomEl: false,
-          index: index,
-
-          showAnimationDuration: 0,
-          hideAnimationDuration: 0
-        };
-        $('body').append($.parseHTML(photoswipeTemplate()));
-        var gallery = new PhotoSwipe(document.querySelectorAll('.pswp')[0], PhotoSwipeUI_Default, items, options);
-        gallery.init();
-
-      }
     }
 
     wall.fitWidth();
@@ -72,4 +42,3 @@ Fliplet.Widget.instance('image-gallery', function (data) {
 
   initGallery();
 });
-
